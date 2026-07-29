8d658b2691e5741a6fb5fdd89c3ebf44ca6d2eb5
Đặng Hữu Ý <hyhihidss@gmail.com>
2026-07-29 21:51:31 +0700

    Update target.h

--- src/main/target/MATEKF405/target.h
+++ src/main/target/MATEKF405/target.h
@@ -21,15 +21,18 @@
 #define SPI1_MISO_PIN           PA6
 #define SPI1_MOSI_PIN           PA7
 
-// --- Cấu hình chuẩn tên Macro cho INAV ---
-#define USE_IMU_ICM20602
-#define IMU_ICM20602_ALIGN      CW0_DEG
-
+// --- BẬT DRIVER CHUẨN INAV CHO ICM20602 ---
+#define USE_GYRO
+#define USE_GYRO_SPI_ICM20602
 #define GYRO_1_CS_PIN           PC2
 #define GYRO_1_SPI_BUS          BUS_SPI1
+#define GYRO_1_ALIGN            CW0_DEG
 
+#define USE_ACC
+#define USE_ACC_SPI_ICM20602
 #define ACC_1_CS_PIN            PC2
 #define ACC_1_SPI_BUS           BUS_SPI1
+#define ACC_1_ALIGN             CW0_DEG
 // *************** UART Config *****************************
 #define USE_VCP
 //#define VBUS_SENSING_PIN        PB12
