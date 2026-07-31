d14077475245547cb859de79397821775c9dc49b
Đặng Hữu Ý <hyhihidss@gmail.com>
2026-07-31 10:14:57 +0700

    Update target.c

--- src/main/target/MATEKF405/target.c
+++ src/main/target/MATEKF405/target.c
@@ -21,23 +21,22 @@
 #include "drivers/pwm_mapping.h"
 #include "drivers/timer.h"
 
-timerHardware_t timerHardware[] = {
-    // DEF_TIM(TIM9, CH2, PA3,  TIM_USE_PPM,   0, 0), // PPM
+// 1. Đăng ký duy nhất 1 Gyro/ACC trên SPI1 (CS = PC2)
+BUSDEV_REGISTER_SPI(
+    BUS_SPI1,
+    DEVHW_MPU6500,
+    SPI1_DEV,
+    GYRO_1_CS_PIN,
+    1,
+    OWNER_MPU
+);
 
-    DEF_TIM(TIM3, CH1, PC6,  TIM_USE_OUTPUT_AUTO, 0, 0), // S1
-    DEF_TIM(TIM8, CH2, PC7,  TIM_USE_OUTPUT_AUTO, 0, 1), // S2  UP(2,1)
-    DEF_TIM(TIM8, CH3, PC8,  TIM_USE_OUTPUT_AUTO, 0, 1), // S3  UP(2,1)
-    DEF_TIM(TIM8, CH4, PC9,  TIM_USE_OUTPUT_AUTO, 0, 0), // S4  UP(2,1)
-
-    DEF_TIM(TIM2, CH1, PA15, TIM_USE_OUTPUT_AUTO, 0, 0), // S5  UP(1,7) -- LED
-
-    DEF_TIM(TIM1, CH1, PA8,  TIM_USE_OUTPUT_AUTO, 0, 0), // S6  UP(2,5)
-    DEF_TIM(TIM4, CH3, PB8,  TIM_USE_OUTPUT_AUTO, 0, 0), // S7  D(1,7)!S5 UP(2,6)
-
-    DEF_TIM(TIM5, CH3, PA2,  TIM_USE_PWM,                                            0, 0), // TX2
-
-    DEF_TIM(TIM5, CH1, PA0,  TIM_USE_PWM,                                            0, 0),   // TX4 UP(1,6) D(1,2)!S1
-    DEF_TIM(TIM5, CH2, PA1,  TIM_USE_PWM,                                            0, 0),   // RX4 UP(1,6) D(1,4)
+// 2. Cấu hình Timer cho 4 cổng Motor ESC trên TIM3 (PC6 - PC9)
+const timerHardware_t timerHardware[] = {
+    DEF_TIM(TIM3, CH1, PC6, TIM_USE_MOTOR, 0, 0),
+    DEF_TIM(TIM3, CH2, PC7, TIM_USE_MOTOR, 0, 0),
+    DEF_TIM(TIM3, CH3, PC8, TIM_USE_MOTOR, 0, 0),
+    DEF_TIM(TIM3, CH4, PC9, TIM_USE_MOTOR, 0, 0),
 };
 
 const int timerHardwareCount = sizeof(timerHardware) / sizeof(timerHardware[0]);
