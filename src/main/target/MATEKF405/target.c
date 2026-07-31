2c0dab27ca2f3638fc78a3e71d1f16b494b5cd6d
Đặng Hữu Ý <hyhihidss@gmail.com>
2026-07-31 10:20:49 +0700

    Update target.c

--- src/main/target/MATEKF405/target.c
+++ src/main/target/MATEKF405/target.c
@@ -1,38 +1,16 @@
-/*
-* This file is part of Cleanflight.
-*
-* Cleanflight is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* Cleanflight is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with Cleanflight.  If not, see <http://www.gnu.org/licenses/>.
-*/
+#include "platform.h"
 
-#include <stdbool.h>
-#include <platform.h>
-#include "drivers/io.h"
-#include "drivers/pwm_mapping.h"
-#include "drivers/timer.h"
-
-// 1. Đăng ký duy nhất 1 Gyro/ACC trên SPI1 (CS = PC2)
+// 1. Đăng ký duy nhất 1 Gyro/ACC trên SPI1 (CS = PC2) - Đúng 5 tham số
 BUSDEV_REGISTER_SPI(
     BUS_SPI1,
     DEVHW_MPU6500,
     SPI1_DEV,
     GYRO_1_CS_PIN,
-    1,
     OWNER_MPU
 );
 
-// 2. Cấu hình Timer cho 4 cổng Motor ESC trên TIM3 (PC6 - PC9)
-const timerHardware_t timerHardware[] = {
+// 2. Cấu hình Timer cho 4 cổng Motor ESC trên TIM3 (PC6 - PC9) - Đã bỏ chữ 'const'
+timerHardware_t timerHardware[] = {
     DEF_TIM(TIM3, CH1, PC6, TIM_USE_MOTOR, 0, 0),
     DEF_TIM(TIM3, CH2, PC7, TIM_USE_MOTOR, 0, 0),
     DEF_TIM(TIM3, CH3, PC8, TIM_USE_MOTOR, 0, 0),
